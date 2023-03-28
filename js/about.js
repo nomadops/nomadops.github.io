1d17d9ff1c0c8c43fe0c36234b3820f4cc723755
SofiiCode <sofi.andand06@gmail.com>
2023-03-28 15:23:19 +0300

    debug navbar

--- js/about.js
+++ js/about.js
@@ -1,6 +1,18 @@
 const img = document.getElementById("img-main");
 
-console.log(img)
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarSupportedContent");
+const bsCollapse = new bootstrap.Collapse(menuToggle);
+
 window.addEventListener("load", function (event) {
   img.classList.add('main-img')
+  menuToggle.classList.remove("show");
 });
+
+   navLinks.forEach((l) => {
+     l.addEventListener("click", () => {
+       bsCollapse.toggle();
+     });
+   });
+
+
