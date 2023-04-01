9c137db353fbe3c92df879c2346d68bab1de325f
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2023-04-02 00:20:41 +0300

    Disables webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require("path");
-const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
-const glob = require("glob-all");
-
-module.exports = {
-  mode: "production",
-  entry: "./src/index.html",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    usedExports: true,
-  },
-  plugins: [
-    new PurgeCSSPlugin({
-      paths: glob.sync([
-        path.join(__dirname, "src/*.html"),
-        path.join(__dirname, "src/**/*.js"),
-      ]),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {
-              minimize: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
+// const path = require("path");
+// const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+// const glob = require("glob-all");
+// 
+// module.exports = {
+//   mode: "production",
+//   entry: "./src/index.html",
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, "dist"),
+//   },
+//   optimization: {
+//     usedExports: true,
+//   },
+//   plugins: [
+//     new PurgeCSSPlugin({
+//       paths: glob.sync([
+//         path.join(__dirname, "src/*.html"),
+//         path.join(__dirname, "src/**/*.js"),
+//       ]),
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.html$/,
+//         use: [
+//           {
+//             loader: "html-loader",
+//             options: {
+//               minimize: true,
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// };
